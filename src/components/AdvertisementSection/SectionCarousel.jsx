f3d0bb442bd6a9cf405ebf8d7cefa7674d578783
summer-bst <453421041@qq.com>
2023-10-11 13:53:24 +0800

    swiper

--- src/components/AdvertisementSection/SectionCarousel.jsx
+++ src/components/AdvertisementSection/SectionCarousel.jsx
@@ -1,109 +1,54 @@
-import React, { useMemo, useRef, useState } from 'react'
+import React, { useMemo } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Navigation } from 'swiper'
+import { Navigation, Autoplay } from 'swiper/modules'
 import classNames from 'classnames'
-import FormattedText from '@/components/common/FormattedText/Index'
-import SwiperSwitch from '@/components/adapter/swiperSwitch/Index'
-import BaseHeader from '../common/Heading/BaseHeader'
 import Advertisement from './Advertisement'
 import './sectionCarousel.scss'
-SwiperCore.use([Navigation])
 const SectionCarousel = ({
   className,
   list,
-  carouselKeyId = 'carousel',
   slideConfig = {},
   slidesPerView = 1,
   slidesPerGroup,
 }) => {
-  const nextElClassName = `swiper-button-next_section_${carouselKeyId}`
-  const prevElClassName = `swiper-button-prev_section_${carouselKeyId}`
-  const swiperInstanceRef = useRef(null)
   const settings = useMemo(() => {
     return {
-      simulateTouch: false,
+      modules: [Navigation, Autoplay],
+      centeredSlides: true,
+      loop: true,
+      onAfterInit: (swiper) => swiper.slideTo(1, 0),
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
       spaceBetween: 20,
-      navigation: {
-        nextEl: `.${nextElClassName}`,
-        prevEl: `.${prevElClassName}`,
-        disabledClass: 'disabled',
-      },
+      loopedSlides: 5,
+      navigation: true,
       slidesPerView: slidesPerView,
-      slidesPerGroup: slidesPerGroup || slidesPerView,
-      a11y: false,
-      virtual: true,
-      onAfterInit(swiper) {
-        swiperInstanceRef.current = swiper
-      },
-      onSnapIndexChange(swiper) {
-        setFocusIndex(() => {
-          return swiper.snapIndex * swiper.params.slidesPerView
-        })
+      breakpoints: {
+        800: {
+          slidesPerView: 1.5,
+        },
       },
       ...slideConfig,
     }
-  }, [slidesPerView, prevElClassName, nextElClassName, slidesPerGroup, slideConfig])
-
-  const [focusIndex, setFocusIndex] = useState(0)
-  const contentRef = useRef(null)
-
-  const onKeyDown = (evt) => {
-    const delay = 200
-    let newFocusIndex = -1
-    if (evt.keyCode === 39) {
-      if (focusIndex < list.length - 1) {
-        newFocusIndex = focusIndex + 1
-      }
-    } else if (evt.keyCode === 37) {
-      if (focusIndex > 0) {
-        newFocusIndex = focusIndex - 1
-      }
-    }
-
-    if (newFocusIndex > -1) {
-      setFocusIndex(newFocusIndex)
-      swiperInstanceRef.current.slideTo(newFocusIndex)
-      evt.stopPropagation()
-      setTimeout(() => {
-        document.getElementById(`${carouselKeyId}-${newFocusIndex}`).focus()
-      }, delay)
-    }
-  }
+  }, [slidesPerView, slidesPerGroup, slideConfig])
 
   if (list.length === 0) {
     return null
   }
 
   return (
-    <section
-      className={classNames(['section-carousel-container', className])}
-      aria-labelledby='featuredGame'
-    >
-      <BaseHeader className='section-carousel-header' data-focus-block>
-        <span id='featuredGame'>
-          <FormattedText className='section-carousel-headerTitle' id='featuredGame' />
-        </span>
-        {list.length > slidesPerView ? (
-          <SwiperSwitch prevElClassName={prevElClassName} nextElClassName={nextElClassName} />
-        ) : null}
-      </BaseHeader>
-      <main onKeyDown={onKeyDown} data-focus-block>
-        <Swiper {...settings} className='section-carousel-body' ref={contentRef}>
-          {list.map((banner, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <Advertisement
-                  app={banner}
-                  showInstalledState
-                  className='section-carousel-item'
-                  tabIndex={focusIndex === index ? 0 : -1}
-                  tabId={`${carouselKeyId}-${index}`}
-                />
-              </SwiperSlide>
-            )
-          })}
-        </Swiper>
-      </main>
+    <section className={classNames(['section-carousel-container', className])}>
+      <Swiper {...settings} className='section-carousel-body'>
+        {list.map((banner, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <Advertisement app={banner} className='section-carousel-item' />
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
     </section>
   )
 }
--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import HomeBanner from './components/banner/Index'
 import { useGridBaseCount } from '@/hook/useViewport'
 // import Categorys from './components/categorys/Index'
+import GameSwiper from './components/swiper/Index'
+import SectionCarousel from '@/components/AdvertisementSection/SectionCarousel'
 
 import './index.scss'
 // 记录页面的状态
 const Home = () => {
   // 云游戏
   const gridCount = useGridBaseCount()
-
+  const apps = useSelector((state) => state.app.html5) || []
   return (
     <div className='home-container'>
+      {/* <GameSwiper /> */}
+      <SectionCarousel list={apps} className='margin-bottom-30' />
       <HomeBanner gridCount={gridCount} className=' margin-bottom-50' />
       {/* category */}
       {/* <Categorys /> */}
